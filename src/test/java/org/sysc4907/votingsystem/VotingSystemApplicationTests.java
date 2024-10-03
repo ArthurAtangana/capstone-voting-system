096665b27b2c6a8425623165f92aec7de68e8928
Jasmine Gad El Hak <itsjessyg@gmail.com>
2024-10-03 15:26:08 -0400

    Setting up maven dependencies

---
+++ src/test/java/org/sysc4907/votingsystem/VotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sysc4907.votingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
