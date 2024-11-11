b551e056ea99421dd030a79c01597703470a681c
Jasmine Gad El Hak <itsjessyg@gmail.com>
2024-11-11 17:40:25 -0500

    Moved to new directory

--- src/main/java/org/sysc4907/votingsystem/Accounts/AccountService.java
+++ src/main/java/org/sysc4907/votingsystem/Accounts/AccountService.java
@@ -1,4 +1,4 @@
-package org.sysc4907.votingsystem.Elections;
+package org.sysc4907.votingsystem.Accounts;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
