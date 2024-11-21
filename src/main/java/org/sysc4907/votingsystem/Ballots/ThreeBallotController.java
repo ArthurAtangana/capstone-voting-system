89368b0341ed005e24a0b118f9d3f4cc9215789c
Rebecca-Elliott <rebeccaelliott35@gmail.com>
2024-11-21 15:47:42 -0500

    Take in candidates as a parameter to make ballots

--- src/main/java/org/sysc4907/votingsystem/Ballots/ThreeBallotController.java
+++ src/main/java/org/sysc4907/votingsystem/Ballots/ThreeBallotController.java
@@ -1,17 +1,22 @@
 package org.sysc4907.votingsystem.Ballots;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Arrays;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.*;
 
 @Controller
 public class ThreeBallotController {
 
     @GetMapping("/threeBallot")
-    public String threeBallot(Model model) {
-        ThreeBallot threeBallot = new ThreeBallot(Arrays.asList("foo", "bar", "baz", "quux"));
+    public String threeBallot(@RequestParam String candidates, Model model){
+        // Split by comma, then collect into a List
+        List<String> candidatesAsList = Arrays.stream(candidates
+                        .split(","))
+                .map(String::trim)  // Trim any extra spaces
+                .toList();
+
+        ThreeBallot threeBallot = new ThreeBallot(candidatesAsList);
         model.addAttribute("threeBallot", threeBallot);
         model.addAttribute("firstBallotMarks", threeBallot.getFirstBallot().getMarkValues());
         model.addAttribute("secondBallotMarks", threeBallot.getSecondBallot().getMarkValues());
