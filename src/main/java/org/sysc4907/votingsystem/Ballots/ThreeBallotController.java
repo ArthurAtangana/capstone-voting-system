c1cf148725bb0bdb877369882b2d97c3cc9af6a9
Victoria Malouf <victoriamalouf@cmail.carleton.ca>
2025-01-14 16:55:35 -0500

    adding buttons

--- src/main/java/org/sysc4907/votingsystem/Ballots/ThreeBallotController.java
+++ src/main/java/org/sysc4907/votingsystem/Ballots/ThreeBallotController.java
@@ -2,7 +2,6 @@ package org.sysc4907.votingsystem.Ballots;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.sysc4907.votingsystem.Elections.ElectionService;
@@ -16,25 +15,9 @@ public class ThreeBallotController {
     @GetMapping("/threeBallotTest")
     public String threeBallotTest(Model model) {
         ThreeBallot threeBallot = new ThreeBallot(Arrays.asList("foo", "bar", "baz", "quux"));
-        model.addAttribute("threeBallot", threeBallot);
-        // Extract the IDs from the ballots
         List<Map<String, Object>> attributes = threeBallot.getBallotAttributes();
         model.addAttribute("attributes", attributes);
-
-        for (Map<String, Object> attribute : attributes) {
-            System.out.println("Ballot: ");
-            for (Map.Entry<String, Object> entry : attribute.entrySet()) {
-                // Print the markableBoxes array in a readable format
-                if (entry.getKey().equals("markableBoxes")) {
-                    boolean[] markValues = (boolean[]) entry.getValue(); // Assuming it's a boolean array
-                    String marksReadable = Arrays.toString(markValues); // Converts the array to a string representation
-                    System.out.println("Markable Boxes: " + marksReadable);
-                } else {
-                    System.out.println(entry.getKey() + ": " + entry.getValue());
-                }
-            }
-        }
-
+        model.addAttribute("threeBallot", threeBallot);
         model.addAttribute("message", "hey");
         model.addAttribute("firstBallotMarks", threeBallot.getFirstBallot().getMarkValues());
         model.addAttribute("secondBallotMarks", threeBallot.getSecondBallot().getMarkValues());
@@ -46,6 +29,8 @@ public class ThreeBallotController {
     public String threeBallot(Model model) {
         List<String> candidates = electionService.getElection().getCandidates();
         ThreeBallot threeBallot = new ThreeBallot(candidates);
+        List<Map<String, Object>> attributes = threeBallot.getBallotAttributes();
+        model.addAttribute("attributes", attributes);
         model.addAttribute("threeBallot", threeBallot);
         model.addAttribute("firstBallotMarks", threeBallot.getFirstBallot().getMarkValues());
         model.addAttribute("secondBallotMarks", threeBallot.getSecondBallot().getMarkValues());
