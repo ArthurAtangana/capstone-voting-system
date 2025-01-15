3f6af68d8752eb18a5ab8160a47fc6441f4bdac4
VictoriaMalouf <84146479+VictoriaMalouf@users.noreply.github.com>
2025-01-14 20:41:58 -0500

    3-ballot receipt page (#45)
    * receipt ballot view
    
    * removing params
    
    * ballot id and canidate order to front end
    
    * receipt changes
    
    * instructions page
    
    * updating markers
    
    * adding buttons
    
    * removed test line
    
    * fixed attribute constant definition

--- src/main/java/org/sysc4907/votingsystem/Ballots/ThreeBallotController.java
+++ src/main/java/org/sysc4907/votingsystem/Ballots/ThreeBallotController.java
@@ -2,7 +2,6 @@ package org.sysc4907.votingsystem.Ballots;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.sysc4907.votingsystem.Elections.ElectionService;
@@ -16,6 +15,8 @@ public class ThreeBallotController {
     @GetMapping("/threeBallotTest")
     public String threeBallotTest(Model model) {
         ThreeBallot threeBallot = new ThreeBallot(Arrays.asList("foo", "bar", "baz", "quux"));
+        List<Map<String, Object>> attributes = threeBallot.getBallotAttributes();
+        model.addAttribute("attributes", attributes);
         model.addAttribute("threeBallot", threeBallot);
         model.addAttribute("firstBallotMarks", threeBallot.getFirstBallot().getMarkValues());
         model.addAttribute("secondBallotMarks", threeBallot.getSecondBallot().getMarkValues());
@@ -27,10 +28,17 @@ public class ThreeBallotController {
     public String threeBallot(Model model) {
         List<String> candidates = electionService.getElection().getCandidates();
         ThreeBallot threeBallot = new ThreeBallot(candidates);
+        List<Map<String, Object>> attributes = threeBallot.getBallotAttributes();
+        model.addAttribute("attributes", attributes);
         model.addAttribute("threeBallot", threeBallot);
         model.addAttribute("firstBallotMarks", threeBallot.getFirstBallot().getMarkValues());
         model.addAttribute("secondBallotMarks", threeBallot.getSecondBallot().getMarkValues());
         model.addAttribute("thirdBallotMarks", threeBallot.getThirdBallot().getMarkValues());
         return "three-ballot";
+    }
+
+    @GetMapping("/instructions")
+    public String showInstructionsPage() {
+        return "instructions";
     }
 }
